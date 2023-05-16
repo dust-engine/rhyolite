1029c3c18ffb0f8b9d5dfd05144a8817bec4e4d3
Zhixing Zhang <me@neoto.xin>
2023-05-16 06:52:19 -0700

    fixes

--- core/src/resources/managed_buffer.rs
+++ core/src/resources/managed_buffer.rs
@@ -669,7 +669,7 @@ impl ManagedBufferStrategyStagingUnsized {
                 });
 
             let changes = std::mem::take(&mut self.changes);
-            let ordered_change_buffer = staging_buffer.contents_mut().unwrap();
+            let ordered_change_buffer = &mut staging_buffer.contents_mut().unwrap()[0..self.change_buffer.len()];
             {
                 let mut ordered_change_buffer_len = 0;
                 for (_, change_buffer_index) in changes.iter() {
